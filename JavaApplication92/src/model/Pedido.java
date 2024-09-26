c0f1433bb4a24edfcae7e74379312cc03eae6dfe
HigorLegal <higorgom002@gmail.com>
2024-09-26 19:24:59 -0300

    omasc

--- JavaApplication92/src/model/Pedido.java
+++ JavaApplication92/src/model/Pedido.java
@@ -10,5 +10,43 @@ package model;
  * @author S.lucas
  */
 public class Pedido {
+
+    private int pkpedido;
+    private int fkusuario;
+    private int fkproduto;
+
+    public Pedido(int pkpedido, int fkusuario, int fkproduto) {
+        this.pkpedido = pkpedido;
+        this.fkusuario = fkusuario;
+        this.fkproduto = fkproduto;
+    }
+
+    public Pedido() {
+    }
+
+    public int getPkpedido() {
+        return pkpedido;
+    }
+
+    public void setPkpedido(int pkpedido) {
+        this.pkpedido = pkpedido;
+    }
+
+    public int getFkusuario() {
+        return fkusuario;
+    }
+
+    public void setFkusuario(int fkusuario) {
+        this.fkusuario = fkusuario;
+    }
+
+    public int getFkproduto() {
+        return fkproduto;
+    }
+
+    public void setFkproduto(int fkproduto) {
+        this.fkproduto = fkproduto;
+    }
     
+
 }
